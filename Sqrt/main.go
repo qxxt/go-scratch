e043361ed7aa77e5df262a2ac9ce121c1ebd1ced
qxxt <57898942+qxxt@users.noreply.github.com>
2023-06-03 13:40:47 +0700

    SquareRoot

---
+++ Sqrt/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	testData := []int{2, 4, 8, 64, 984230921}
+	for _, v := range testData {
+		fmt.Println("Target:", v)
+		fmt.Println("Square Root:", mySqrt(v))
+		fmt.Println("Square Root Long:", mySqrtFloat(float64(v)))
+	}
+}
+
+func mySqrtFloat(x float64) float64 {
+	res := x / 2
+	for z := 0.0; z != res; {
+		z = res
+		res = (res + x/res) / 2
+	}
+	return res
+}
+
+func mySqrt(x int) int {
+	res := x
+	for res*res > x {
+		res = (res + x/res) / 2
+	}
+	return res
+}
